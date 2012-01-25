c5f69031cc198b0b013df8a2dd2b369be9c0a727
pierreetienne.meunier <pierreetienne.meunier@gmail.com>
2012-01-25 22:07:54 +0000

    List functions

--- Binary.ml
+++ Binary.ml
@@ -12,6 +12,28 @@ let round x=
       
 let is_infinite x=match classify_float x with FP_infinite->true | _->false
 
+let rec span f=function
+    []->([],[])
+  | h::s when f h->let (a,b)=span f s in (h::a, b)
+  | l->([],l)
+
+let rec break f=function
+    []->([],[])
+  | h::s when not (f h)->let (a,b)=break f s in (h::a, b)
+  | l->([],l)
+
+let rec take x l=
+  if x<=0 then [] else
+    match l with
+        []->[]
+      | h::s->h::(take (x-1) s)
+
+let rec drop x l=
+  if x<=0 then l else
+    match l with
+        []->[]
+      | _::s->drop (x-1) l
+          
 module IntMap=Map.Make (struct type t=int let compare=compare end)
 module StrMap=Map.Make (String)
 module IntSet=Set.Make (struct type t=int let compare=compare end)
