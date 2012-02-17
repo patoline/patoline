4f7940fc5a4239b88e09a6b9f708f8a686fbcaa0
pierre.hyvernat <pierre.hyvernat@univ-savoie.fr>
2012-02-17 10:01:14 +0000

    minimalist interface for graphics_font

--- tests/graphics_font.ml
+++ tests/graphics_font.ml
@@ -1,9 +1,11 @@
 open Fonts.FTypes
 open Fonts.Opentype
-let f="AGaramondPro-Regular.otf"
 
-let font=loadFont f
+let f = try Sys.argv.(1) with _ -> "AGaramondPro-Regular.otf"
+let initial_glyph = try int_of_string Sys.argv.(2) with _ -> 0
 
+let font= loadFont f
+let nb_glyphs = cardinal font
 let round x=(int_of_float (x/.2.))
 
 let x=read_lookup font 4
@@ -12,16 +14,17 @@ let x=read_lookup font 4
 let _=
   let xx0=100 in
   let yy0=100 in
-  (*  let arr= [| 34 ; 35 ; 36 ; 36 ; 37 ; 115 ; 116; 911; 912; 913 |] in *)
-  let arr = Array.init 935 (fun i -> i) in
+  let arr = Array.init nb_glyphs (fun i -> i) in
     Graphics.open_graph "";
     let rec show_glyphs i=
       let gl=loadGlyph font { empty_glyph with glyph_index=arr.(i mod (Array.length arr)) } in
       Graphics.clear_graph ();
+
       Graphics.set_color (Graphics.rgb 150 150 150);
       Graphics.moveto 5 5; Graphics.draw_string "'n': +1  'N': +50  'p': -1 'P': -50  'q': quit";
-      Graphics.moveto 10 20; Graphics.draw_string ("glyph " ^ (string_of_int (glyphNumber gl).glyph_index));
+      Graphics.moveto 10 20; Graphics.draw_string (f ^ ", glyph " ^ (string_of_int (glyphNumber gl).glyph_index) ^ " / " ^ (string_of_int nb_glyphs));
       Graphics.set_color Graphics.black;
+
       Graphics.moveto 0 yy0;Graphics.lineto (Graphics.size_x()) yy0;
       let out=outlines gl in
         List.iter (fun (x,y)->
@@ -47,4 +50,4 @@ let _=
            | ' ' | 'n' -> show_glyphs (i+1)
            | _ -> show_glyphs (i+1)
     in
-      show_glyphs 0
+      show_glyphs initial_glyph
