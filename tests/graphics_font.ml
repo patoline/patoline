a557cdfb2643452595ad7f5a672f515b537b7a47
pierreetienne.meunier <pierreetienne.meunier@gmail.com>
2012-02-21 10:44:14 +0000

    Optimilations du rasterizer

--- tests/graphics_font.ml
+++ tests/graphics_font.ml
@@ -41,57 +41,67 @@ let basic_draw xx0 yy0 glyph=
 
 let draw_glyph xx0 yy0 glyph=
   let eps=0.01 in
-  let minx=List.fold_left (List.fold_left (fun a (fx,_)->Array.fold_left min a fx)) infinity glyph in
-  let maxx=List.fold_left (List.fold_left (fun a (fx,_)->Array.fold_left max a fx)) (-.infinity) glyph in
+  (* let minx=List.fold_left (List.fold_left (fun a (fx,_)->Array.fold_left min a fx)) infinity glyph in *)
+  (* let maxx=List.fold_left (List.fold_left (fun a (fx,_)->Array.fold_left max a fx)) (-.infinity) glyph in *)
   let miny=List.fold_left (List.fold_left (fun a (_,fy)->Array.fold_left min a fy)) infinity glyph in
   let maxy=List.fold_left (List.fold_left (fun a (_,fy)->Array.fold_left max a fy)) (-.infinity) glyph in
-  let lines=List.map (fun l->List.fold_right (fun (fx,fy) lines->
-                                        if Array.length fx=2 then (
-                                          (fx.(0),fy.(0),fx.(1),fy.(1))::lines
-                                        ) else (
-                                          let rec draw t x y lines0=
-                                            if t<1. then (
-                                              let x'=eval fx t in
-                                              let y'=eval fy t in
-                                                draw (t+.eps) x' y' ((x,y,x',y')::lines0)
-                                            ) else (
-                                              (x,y,fx.(Array.length fx-1),fy.(Array.length fy-1))::lines0
-                                            )
-                                          in
-                                            draw eps fx.(0) fy.(0) lines
-                                        )
-                                     ) l []) glyph
+  let lines=List.map (fun l->
+                        let path=Array.of_list
+                          (List.rev (List.fold_right (fun (fx,fy) lines->
+                                                        if Array.length fx=2 then (
+                                                          (fx.(0),fy.(0),fx.(1),fy.(1))::lines
+                                                        ) else (
+                                                          let rec draw t x y lines0=
+                                                            if t<1. then (
+                                                              let x'=eval fx t in
+                                                              let y'=eval fy t in
+                                                                draw (t+.eps) x' y' ((x,y,x',y')::lines0)
+                                                            ) else (
+                                                              (x,y,fx.(Array.length fx-1),fy.(Array.length fy-1))::lines0
+                                                            )
+                                                          in
+                                                            draw eps fx.(0) fy.(0) lines
+                                                        )
+                                                     ) l []))
+                        in
+                        let arr=Array.make (int_of_float (ceil maxy)-int_of_float (floor miny)+1) [] in
+                          for i=0 to Array.length path-1 do
+                            let _,y0,_,y1=path.(i) in
+                              for j=int_of_float (floor (min y0 y1)) to int_of_float (ceil (max y0 y1)) do
+                                arr.(j-int_of_float (floor miny))<-i::arr.(j-int_of_float (floor miny))
+                              done
+                          done;
+                          (path,arr)
+                     ) glyph
   in
-  let rec inter y lines i result=
-    if i>=Array.length lines then result else (
-      let (x0,y0,x1,y1) = lines.(i) in
-        if (min y0 y1)>y || (max y0 y1)<y then (
-          inter y lines (i+1) result
-        ) else (
-          if y=148. then (Printf.printf "%f %f %f %f\n" x0 y0 x1 y1; flush stdout);
-          let result'=
-            if y0=y then (
-              let _,y0',_,_=lines.((i-1+Array.length lines) mod (Array.length lines)) in
-              let y1'=ref y1 in
-              let j=ref (i+1) in
-                while !y1'=y0 && (!j mod (Array.length lines))<>i do
-                  let _,y0',_,_=lines.(!j mod (Array.length lines)) in
-                    y1':=y0';
-                    incr j
-                done;
-                if (y0'-.y0)*.(!y1'-.y0) < 0. then x0::result else result
-            ) else
-              if y<>y1 then (x0+.(y-.y0)*.(x1-.x0)/.(y1-.y0))::result else result
-          in
-            inter y lines (i+1) result'
-        )
-    )
+  let rec inter y lines is result=match is with
+      []->result
+    | i::s->(
+        let (x0,y0,x1,y1) = lines.(i) in
+          if (min y0 y1)>y || (max y0 y1)<y then (
+            inter y lines s result
+          ) else (
+            let result'=
+              if y0=y then (
+                let _,y0',_,_=lines.((i-1+Array.length lines) mod (Array.length lines)) in
+                let y1'=ref y1 in
+                let j=ref (i+1) in
+                  while !y1'=y0 && (!j mod (Array.length lines))<>i do
+                    let _,y0',_,_=lines.(!j mod (Array.length lines)) in
+                      y1':=y0';
+                      incr j
+                  done;
+                  if (y0'-.y0)*.(!y1'-.y0) < 0. then x0::result else result
+              ) else
+                if y<>y1 then (x0+.(y-.y0)*.(x1-.x0)/.(y1-.y0))::result else result
+            in
+              inter y lines s result'
+          )
+      )
   in
     for y = int_of_float (floor miny) to int_of_float (ceil maxy) do
-      let revLines=List.map (fun p->Array.of_list (List.rev p)) lines in
-      let inters=List.concat (List.map (fun l-> inter (foi y) l 0 []) revLines) in
+      let inters=List.concat (List.map (fun (l,arr)-> inter (foi y) l arr.(y-int_of_float (floor miny)) []) lines) in
       let inter=List.sort compare inters in
-        if y=148 then (List.iter (Printf.printf "%f ") inter; flush stdout);
       let rec remove_dupl=function
           []-> []
         | h::h'::s when h=h' -> remove_dupl (h'::s)
@@ -108,19 +118,11 @@ let draw_glyph xx0 yy0 glyph=
               Graphics.plot (xx0+(int_of_float (floor x0))) (yy0+y);
               Graphics.set_color (Graphics.rgb g1 g1 g1);
               Graphics.plot (xx0+(int_of_float (ceil x1))) (yy0+y);
-
-            draw s
+              draw s
           )
         | _-> ()
       in draw (inter)
-    done;
-    let x=141 in
-    let y=148 in
-      Graphics.set_color Graphics.red;
-      Graphics.moveto (xx0+x) yy0;
-      Graphics.lineto (xx0+x) (yy0+500);
-      Graphics.moveto xx0 (yy0+y);
-      Graphics.lineto (xx0+800) (yy0+y)
+    done
 
 
 let bezier3=Graphics.curveto
@@ -130,7 +132,7 @@ let _=
   let yy0=100 in
   let arr = Array.init nb_glyphs (fun i -> i) in
     Graphics.open_graph "";
-    (* Graphics.auto_synchronize false; *)
+    Graphics.auto_synchronize false;
     let rec show_glyphs i=
       let gl=loadGlyph font { empty_glyph with glyph_index=arr.(i mod (Array.length arr)) } in
       Graphics.clear_graph ();
