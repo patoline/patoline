64f7f8e6ca249c8ef66cbce45b1e508f6d8db170
raffalli <raffalli@univ-savoie.fr>
2012-03-08 08:13:56 +0000

    Minor change in language

--- DefaultFormat.ml
+++ DefaultFormat.ml
@@ -17,10 +17,25 @@ let ragged_right a b c line=
     measure=line.nom_width;
     left_margin=par.left_margin+.par.measure-.line.nom_width }
 
+let title str =
+  let mcenter a b c l =
+    { (center a b c l) with
+      min_height_after = 3 }
+  in
+  newPar (normal 150.) mcenter [size 10. [T str] ]
+
 let author str =
-  newPar (normal 150.) center [size 7. [T str] ]
+  let mcenter a b c l =
+    { (center a b c l) with
+      min_height_after = 1 }
+  in
+  newPar (normal 150.) mcenter [size 6. [T str] ]
 
 let institute str =
-  newPar (normal 150.) center [size 4. [T str] ]
+  let mcenter a b c l =
+    { (center a b c l) with
+      min_height_after = 1 }
+  in
+  newPar (normal 150.) mcenter [size 4. [T str] ]
 
 let textWidth = normal 150.
