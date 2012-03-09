43875805851d64b5dd17af293805eaa988e44595
raffalli <raffalli@univ-savoie.fr>
2012-03-09 14:52:12 +0000

    Macro inside paragraph
    \toto{..}{{..}} must produce "content list" and can be used inside paragraphs.
    
    \Begin...
    \End...
    \Toto
    
    are outside paragraph
    
    \Toto args should open a module Toto (yet to be done) and call a function
    init_Toto args.

--- Texprime.ml
+++ Texprime.ml
@@ -36,6 +36,39 @@ let postambule : ('a, 'b, 'c) format = "
     Drivers.Pdf.output ~structure:(make_struct v !str) u \"%s.pdf\" 
 " 
 
+let rec print_macro ch op mtype name args =
+  begin
+    match mtype with
+    | `Single -> Printf.fprintf ch "%s" name
+    | `Begin -> Printf.fprintf ch "module TEMP = struct\n open Env_%s;;\n do_begin_%s" name name
+    | `End -> Printf.fprintf ch "do_end_%s" name
+  end;
+  List.iter (function
+    Paragraph p -> Printf.fprintf ch " %a" (print_contents op) p
+  | Caml(s,e) ->
+    let size = e - s in
+    let buf=String.create size in
+    let _= seek_in op s; input op buf 0 size in
+    Printf.fprintf ch " (%s)" buf
+  | _ -> assert false) args;
+  if args = [] then Printf.fprintf ch " ()";
+  if mtype = `End then Printf.fprintf ch "\nend"
+
+and print_contents op ch l = 
+  Printf.fprintf ch "(";
+  begin match l with
+    [] ->  Printf.fprintf ch "[]";
+  | TC s :: l -> 
+    Printf.fprintf ch "(T \"%s\")::" (String.escaped s);
+    print_contents op ch l
+  | MC(mtype, name, args) :: l -> 
+    Printf.fprintf ch "(";
+    print_macro ch op mtype name args;
+    Printf.fprintf ch ")@";
+    print_contents op ch l
+  end;
+  Printf.fprintf ch ")"
+
 let _=
   let filename=ref [] in
     Arg.parse spec (fun x->filename:=x::(!filename)) "Usage :";
@@ -54,47 +87,32 @@ let _=
 		  begin match pre with
 		    None -> ()
 		  | Some(title, at) -> 
-		      Printf.printf "title %b %a;;\n\n" (at = None) print_contents title;
+		      Printf.printf "title %b %a;;\n\n" (at = None) (print_contents op) title;
 		      match at with
 			None -> ()
 		      | Some(auth,inst) ->
-			Printf.printf "author %b %a;;\n\n" (inst = None) print_contents auth;
+			Printf.printf "author %b %a;;\n\n" (inst = None) (print_contents op) auth;
 			  match inst with
 			    None -> ()
 			  | Some(inst) ->
-			    Printf.printf "institute true %a;;\n\n" print_contents inst
+			    Printf.printf "institute true %a;;\n\n" (print_contents op) inst
 		  end;
 		  let rec output_list docs = List.iter output_doc docs
 		  and output_doc = function
 		    | Paragraph p ->
 		      Printf.printf "newPar textWidth parameters %a;;\n" 
-			print_contents p
+			(print_contents op) p
 		    | Caml(s,e) ->
 		      let size = e - s in
 		      let buf=String.create size in
 		      let _= seek_in op s; input op buf 0 size in
 		      Printf.printf "%s;;\n\n" buf
 		    | Struct(title, docs) ->
-		      Printf.printf "newStruct %a;;\n\n" print_contents title;
+		      Printf.printf "newStruct %a;;\n\n" (print_contents op) title;
 		      output_list docs;
 		      Printf.printf "up();;\n\n"
 		    | Macro(mtype, name, args) ->
-		      begin
-			match mtype with
-			| `Single -> Printf.printf "%s" name
-			| `Begin -> Printf.printf "module TEMP = struct\n open Env_%s;;\n do_begin_%s" name name
-			| `End -> Printf.printf "do_end_%s" name
-		      end;
-		      List.iter (function
-		        Paragraph p -> Printf.printf " %a" print_contents p
-		      | Caml(s,e) ->
-			let size = e - s in
-			let buf=String.create size in
-			let _= seek_in op s; input op buf 0 size in
-			Printf.printf " (%s)" buf
-		      | _ -> assert false) args;
-		      if args = [] then Printf.printf " ()";
-		      if mtype = `End then Printf.printf "\nend";
+		      print_macro stdout op mtype name args;
 		      Printf.printf ";;\n\n" 
 		  in
 		  output_list docs;
