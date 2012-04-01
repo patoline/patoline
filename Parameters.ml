bfd701ba14b6f1afaf98c6131f9cbaa82b003ad1
pierreetienne.meunier <pierreetienne.meunier@gmail.com>
2012-04-01 09:27:33 +0000

    Rollback

--- Parameters.ml
+++ Parameters.ml
@@ -49,28 +49,15 @@ let normal mes0 paragraphs figures last_users line allow_impossible=
           match paragraphs.(line.paragraph).(j) with
 
               Glue _ when (sum_min <= measure || allow_impossible) && j>line.lineStart ->
-                let rec zero_width ii=
-                  let (_,_,c')=box_interval paragraphs.(line.paragraph).(ii) in
-                    if c'=0. then
-                      zero_width (ii+1)
-                    else
-                      break_next (ii+1) (sum_min+.a) (sum_nom+.b) (sum_max+.c)
-                        ({ line with lineEnd=ii; hyphenEnd=(-1); min_width=sum_min;
-                             nom_width=sum_nom; max_width=sum_max
-                         }::(if allow_impossible then [] else result0))
-                in
-                  zero_width j
+                break_next (j+1) (sum_min+.a) (sum_nom+.b) (sum_max+.c)
+                  ({ line with lineEnd=j; hyphenEnd=(-1); min_width=sum_min;
+                       nom_width=sum_nom; max_width=sum_max
+                   }::(if allow_impossible then [] else result0))
+
             | Glue _ when allow_impossible && result0=[] && j>line.lineStart ->
-                let rec zero_width ii=
-                  let (_,_,c')=box_interval paragraphs.(line.paragraph).(ii) in
-                    if c'=0. then
-                      zero_width (ii+1)
-                    else
-                      [{ line with lineEnd=ii; hyphenEnd=(-1); min_width=sum_min;
-                           nom_width=sum_nom; max_width=sum_max
-                       }]
-                in
-                  zero_width j
+                [{ line with lineEnd=j; hyphenEnd=(-1); min_width=sum_min;
+                     nom_width=sum_nom; max_width=sum_max
+                 }]
 
             | _ when sum_min<=measure || (allow_impossible && result0=[]) ->
                 break_next (j+1) (sum_min+. a) (sum_nom+.b) (sum_max+. c) result0
