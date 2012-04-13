14778fe8a54aa5efb6b379b108cf844a18b2a32f
pierreetienne.meunier <pierreetienne.meunier@gmail.com>
2012-04-13 20:26:33 +0000

    fonctions auxiliaires

--- src/Typography/Binary.ml
+++ src/Typography/Binary.ml
@@ -61,6 +61,50 @@ let rec drop x l=
         []->[]
       | _::s->drop (x-1) s
 
+let rec last=function
+    [h]->h
+  | _::s->last s
+  | _->raise Not_found
+
+let rec init=function
+    [_] | [] ->[]
+  | h::s->h::init s
+
+let is_space x0=
+  let x=UChar.uint_code x0 in
+    (x>=0x0009 && x<=0x000d)
+  || x=0x0020
+  || x=0x0085
+  || x=0x00a0
+  || x=0x1680
+  || x=0x180e
+  || (x>=0x2000 && x<=0x200a)
+  || x=0x2028 || x=0x2029
+  || x=0x202f
+  || x=0x205f
+  || x=0x3000
+
+let unspace s=
+  let rec rem i j=
+    if i>j then "" else
+      if is_space (UTF8.get s i) then
+        rem (UTF8.next s i) j
+      else
+        if is_space (UTF8.get s j) then
+          rem i (UTF8.prev s j)
+        else
+          String.sub s i (j-i+1)
+  in
+    rem (UTF8.first s) (UTF8.last s)
+
+let rec split f n i0 i=
+  if i0>=String.length n then [] else
+    if f (UTF8.get n i) || i>=String.length n then
+      (String.sub n i0 (i-i0)) :: (split f n (UTF8.next n i) (UTF8.next n i))
+    else
+      split f n i0 (UTF8.next n i)
+
+
 let concat_utf8 a b=
   let x=UTF8.Buf.create (UTF8.length a+UTF8.length b) in
     UTF8.Buf.add_string x a;
