a47014eb69a204cbe063aec6208c0d9193efc5b4
pierreetienne.meunier <pierreetienne.meunier@gmail.com>
2012-05-12 21:07:15 +0000

    ca ne coutait meme pas beaucoup plus cher de rajouter les references vers des labels. Vers les figures, c'est un peu plus dur, par contre, donc ce n'est pas encore fait

--- src/Typography/Output/OutputPaper.ml
+++ src/Typography/Output/OutputPaper.ml
@@ -23,12 +23,17 @@ end
 module Output=functor(M:Driver)->struct
   let output structure paragraphs (figures:drawingBox array) env (opt_pages:(parameters*line) list array) file=
     let positions=Array.make (Array.length paragraphs) (0,0.,0.) in
+    let pages=Array.make (Array.length opt_pages) { pageFormat=a4 ; pageContents=[] } in
+      for i=0 to Array.length opt_pages-1 do pages.(i)<- { pageFormat=a4 ; pageContents=[] } done;
     let par=ref (-1) in
+    let crosslinks=ref [] in (* (page, link, destination) *)
+    let crosslink_opened=ref false in
+    let destinations=ref StrMap.empty in
+    let urilinks=ref None in
     let draw_page i p=
-      let page= { pageFormat=a4 ; pageContents=[] } in
+      let page=pages.(i) in
       let footnotes=ref [] in
       let footnote_y=ref (-.infinity) in
-      let links=ref None in
         List.iter (
           fun (param,line)->
             let y=270.0-.line.height in
@@ -48,12 +53,21 @@ module Output=functor(M:Driver)->struct
 
                 let comp=compression paragraphs param line in
                 let rec draw_box x y box=
-                  (match !links with
-                       None->()
-                     | Some h->(
-                         h.link_y0<-min h.link_y0 (y+.lower_y box 0.);
-                         h.link_y1<-max h.link_y1 (y+.upper_y box 0.)
-                       ));
+                  let lowy=y+.lower_y box 0. in
+                  let uppy=y+.upper_y box 0. in
+                    (match !urilinks with
+                         None->()
+                       | Some h->(
+                           h.link_y0<-min h.link_y0 lowy;
+                           h.link_y1<-max h.link_y1 uppy
+                         ));
+                    if !crosslink_opened then
+                      (match !crosslinks with
+                           []->()
+                         | (_,h,_)::_->(
+                             h.link_y0<-min h.link_y0 lowy;
+                             h.link_y1<-max h.link_y1 uppy
+                           ));
                   match box with
                       Kerning kbox ->(
                         let fact=(box_size kbox.kern_contents/.1000.) in
@@ -79,19 +93,34 @@ module Output=functor(M:Driver)->struct
                           w
                       )
                     | User (BeginURILink l)->(
-                        links:=Some { link_x0=x;link_y0=y;link_x1=x;link_y1=y;uri=l;
-                                      dest_page=0;dest_x=0.;dest_y=0. };
+                        urilinks:=Some { link_x0=x;link_y0=y;link_x1=x;link_y1=y;uri=l;
+                                         dest_page=0;dest_x=0.;dest_y=0. };
                         0.
                       )
+                    | User (BeginLink l)->(
+                        crosslinks:=(i, { link_x0=x;link_y0=y;link_x1=x;link_y1=y;uri="";
+                                          dest_page=0;dest_x=0.;dest_y=0. }, l) :: !crosslinks;
+                        crosslink_opened:=true;
+                        0.
+                      )
+                    | User (Label l)->(
+                        let y0,y1=line_height paragraphs line in
+                          destinations:=StrMap.add l (i,param.left_margin,y+.y0,y+.y1) !destinations;
+                          0.
+                      )
                     | User EndLink->(
-                        (match !links with
-                             None->()
+                        (match !urilinks with
+                             None->(
+                               match !crosslinks with
+                                   []->()
+                                 | (_,h,_)::s->crosslink_opened:=false; h.link_x1<-x
+                             )
                            | Some h->(
                                h.link_x1<-x;
-                               page.pageContents<-Link h::page.pageContents
+                               page.pageContents<-Link h::page.pageContents;
+                               urilinks:=None;
                              )
                         );
-                        links:=None;
                         0.
                       )
                     | User (Footnote (_,g))->(
@@ -101,21 +130,31 @@ module Output=functor(M:Driver)->struct
                       )
                     | b->box_width comp b
                 in
-                  links:=(match !links with
-                              None->None
-                            | Some h->
-                                page.pageContents<-Link h::page.pageContents;
-                                Some { h with link_x0=param.left_margin;link_x1=param.left_margin;
-                                         link_y0=y;link_y1=y });
+                  urilinks:=(match !urilinks with
+                                 None->None
+                               | Some h->
+                                   page.pageContents<-Link h::page.pageContents;
+                                   Some { h with link_x0=param.left_margin;link_x1=param.left_margin;
+                                            link_y0=y;link_y1=y });
+                  if !crosslink_opened then
+                    crosslinks:=(match !crosslinks with
+                                     []->[]
+                                   | (a,h,c)::s->
+                                       (a, { h with link_x0=param.left_margin;link_x1=param.left_margin;
+                                               link_y0=y;link_y1=y }, c)::(a,h,c)::s);
                   let x1=fold_left_line paragraphs (fun x b->x+.draw_box x y b) param.left_margin line in
-                    match !links with
-                        None->()
-                      | Some h->h.link_x1<-x1
+                    (match !urilinks with
+                         None->()
+                       | Some h->h.link_x1<-x1);
+                    if !crosslink_opened then
+                      (match !crosslinks with
+                           []->()
+                         | (_,h,_)::_->h.link_x1<-x1)
               )
         ) p;
-        (match !links with
+        (match !urilinks with
              None->()
-           | Some h->page.pageContents<-Link h::page.pageContents; links:=None);
+           | Some h->page.pageContents<-Link h::page.pageContents; urilinks:=None);
         ignore (
           List.fold_left (
             fun y footnote->
@@ -143,7 +182,15 @@ module Output=functor(M:Driver)->struct
                                 | _ -> x0
                              ) x pnum
         in
-          { page with pageContents=List.rev page.pageContents }
+          page.pageContents<-List.rev page.pageContents
     in
-      M.output ~structure:(make_struct positions structure) (Array.mapi draw_page opt_pages) file
+      for i=0 to Array.length pages-1 do draw_page i opt_pages.(i) done;
+      List.iter (fun (p,link,dest)->try
+                   let (p',x,y0,y1)=StrMap.find dest !destinations in
+                     pages.(p).pageContents<-Link { link with dest_page=p'; dest_x=x; dest_y=y0+.(y1-.y0)*.phi }
+                     ::pages.(p).pageContents
+                 with
+                     Not_found->()
+                ) !crosslinks;
+      M.output ~structure:(make_struct positions structure) pages file
 end
