ac3d9545ff864c68d162cebc38bda60738f7c2ff
Christophe.Raffalli <Christophe.Raffalli@univ-savoie.fr>
2012-06-12 14:28:37 +0000

    FormatArticle compiles ans install
    - FormatArticle does not depend upon Bibi anymore
    - FormatArticle.mllib added

--- src/Format/FormatArticle.ml
+++ src/Format/FormatArticle.ml
@@ -10,38 +10,6 @@ open Line
 open CamomileLibrary
 module CM = CamomileLibraryDefault.Camomile.CaseMap.Make(CamomileLibrary.UTF8)
 
-open Bibi
-exception No_bib of string
-let bib:((string*int*user content list) IntMap.t) ref=ref IntMap.empty
-let bibfile="biblio.bibi"
-let raw_cite x=
-  let num a b=try
-    let _,y,_=(IntMap.find a !bib) in y
-  with
-      Not_found->
-        let key=if IntMap.is_empty !bib then 1 else
-          (let _,(_,b,_)=IntMap.max_binding !bib in
-             b+1)
-        in
-          bib:=IntMap.add a (bibfile,key, b) !bib;
-          key
-  in
-    match Bibi.bibitem bibfile x with
-        []-> raise (No_bib (Printf.sprintf "Request gave no results : %s\n" x))
-      | [a,b]->[T (Printf.sprintf "[%d]" (num a b))]
-      | (a,b)::_::_->(
-          Printf.fprintf stderr "Warning : more than one result for request : %s\n" x;
-          [T (Printf.sprintf "[%d]" (num a b))]
-        )
-
-let cite ?title:(title="") ?year:(year=None) (authors:string list) =
-  raw_cite ("title like '%" ^ title ^ "%' AND id IN " ^
-    (String.concat " AND id IN " 
-       (List.map
-	  (fun author -> 
-	    ("(SELECT article FROM authors_publications WHERE author IN (SELECT id FROM "^
-		"authors WHERE name like '%" ^ author ^ "%'))"))
-	  authors)))
 
 let boxes_width env contents = 
   let boxes = boxify_scoped env contents in
@@ -80,22 +48,6 @@ module Format=functor (D:DocumentStructure)->struct
 
   let caml x = x
   let displayedFormula=Default.displayedFormula
-
-
-let thebibliography ()=
-    List.iter (fun (a,(_,b,c))->
-                 let params env a1 a2 a3 a4 a5 line=
-                   let p=parameters env a1 a2 a3 a4 a5 line in
-                     if line.lineStart=0 then (
-                       let num=boxify_scoped env [T (Printf.sprintf "[%d]" b); (T " ")] in
-                       let w=List.fold_left (fun w0 b->let (_,w,_)=box_interval b in w0+.w) 0. num in
-                         { p with left_margin=p.left_margin-.w; measure=p.measure+.w }
-                     ) else
-        	       p
-                 in
-                   newPar D.structure ~environment:(fun x -> { x with par_indent = [] })
-                     Complete.normal params
-                     (T (Printf.sprintf "[%d]" b)::(T " ")::c)) (IntMap.bindings !bib)
 
 
 module Env_definition=Default.Make_theorem
