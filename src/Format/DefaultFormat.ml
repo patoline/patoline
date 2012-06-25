4bc9fab414d1f4d18293aa2b851b3a69a4bc0586
pierreetienne.meunier <pierreetienne.meunier@gmail.com>
2012-06-25 15:26:35 +0000

    Mouche de figure

--- src/Format/DefaultFormat.ml
+++ src/Format/DefaultFormat.ml
@@ -274,7 +274,7 @@ module Format=functor (D:Document.DocumentStructure)->(
             draw_boxes (
               boxify_scoped env (
                 [ T "Figure"; T " ";
-                  T (String.concat "." (List.map (fun x->string_of_int (x+1)) (List.rev (num@sect_num)))) ]
+                  T (String.concat "." (List.map (fun x->string_of_int x) (List.rev (num@sect_num)))) ]
                 @(if caption=[] then [] else T" "::T"-"::T" "::caption)
               )
             )
