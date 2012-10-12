f2ec02626be1201f71f9d9d846cbaaedd1f37b05
pierreetienne.meunier <pierreetienne.meunier@gmail.com>
2012-10-12 09:33:22 +0000

    Changements de Makefile pour src/Pdf

--- configure.ml
+++ configure.ml
@@ -18,7 +18,7 @@ let lablglut=ref ""
 let lablgtk2=ref ""
 let lang=ref "FR"
 let ban_comic_sans=ref false
-let use_camlimages=ref false
+let use_camlimages=ref true
 
 let avail_lang=
   let f=open_in "src/Typography/TypoLanguage.ml" in
@@ -65,7 +65,7 @@ let _=
     ("--extra-grammars-dir", String (fun pref->grammars_dirs:=pref:: !grammars_dirs), "  additional directories patoline should scan for grammars");
     ("--extra-hyphen-dir", String (fun pref->hyphen_dirs:=pref:: !hyphen_dirs), "  additional directories patoline should scan for hyphenation dictionaries");
     ("--ban-comic-sans", Set ban_comic_sans, " disallows the use of a font with name '*comic*sans*'. Robust to filename changes.");
-    ("--with-camlimages", Set use_camlimages, " allows camlimages experimental library.");
+    ("--without-camlimages", Unit(fun ()->use_camlimages:=false), " allows camlimages experimental library.");
     ("--lang", Set_string lang, Printf.sprintf "  language of the error messages (english by default), available : %s"
        (String.concat ", " (List.rev avail_lang)))
   ] ignore "Usage:";
