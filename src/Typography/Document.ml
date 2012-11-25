1c3b9a4af22d6974c2dd46a9d0a685e341136ec3
pierreetienne.meunier <pierreetienne.meunier@gmail.com>
2012-11-25 19:24:49 +0000

    Corrections d'includeGraphics pour avoir une "bonne" valeur par d?faut de la mise ? l'?chelle.

--- src/Typography/Document.ml
+++ src/Typography/Document.ml
@@ -756,18 +756,16 @@ let notLastLine _=bB (fun _->[Parameters (fun p->{p with not_last_line=true})])
 
 #ifdef CAMLIMAGES
 let image ?scale:(scale=0.) ?width:(width=0.) ?height:(height=0.) imageFile env=
-  Printf.fprintf stderr "->image\n";flush stderr;
   let image=(OImages.load imageFile []) in
-  Printf.fprintf stderr "<-image\n";flush stderr;
   let w,h=Images.size image#image in
   let fw,fh=
     if width=0. then
       if height=0. then
         if scale=0. then
-          if env.normalMeasure<float_of_int w then
+          if env.normalMeasure<(float_of_int w)/.7. then
             env.normalMeasure, env.normalMeasure*.(float_of_int h)/.(float_of_int w)
           else
-            (float_of_int w),(float_of_int h)
+            (float_of_int w)/.7.,(float_of_int h)/.7.
         else
           (float_of_int w)*.scale,(float_of_int h)*.scale
       else
