3a8019b35510e68c6e1091f71f7509059499fd77
pierreetienne.meunier <pierreetienne.meunier@gmail.com>
2012-12-04 10:21:29 +0000

    Copyrights

--- src/Pdf/pdf_parser.ml
+++ src/Pdf/pdf_parser.ml
@@ -1,4 +1,22 @@
-(* module IntMap=Map.Make(struct type t=int let compare=compare end) *)
+(*
+  Copyright Tom Hirschowitz, Florian Hatat, Pierre-Etienne Meunier,
+  Christophe Raffalli and others, 2012.
+
+  This file is part of Patoline.
+
+  Patoline is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Patoline is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Patoline.  If not, see <http://www.gnu.org/licenses/>.
+*)
 (* module IntSet=Set.Make(struct type t=int let compare=compare end) *)
 (* module StrMap=Map.Make(struct type t=string let compare=compare end) *)
 
--- src/Typography/Hyphenate.ml
+++ src/Typography/Hyphenate.ml
@@ -1,3 +1,22 @@
+(*
+  Copyright Tom Hirschowitz, Florian Hatat, Pierre-Etienne Meunier,
+  Christophe Raffalli and others, 2012.
+
+  This file is part of Patoline.
+
+  Patoline is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Patoline is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Patoline.  If not, see <http://www.gnu.org/licenses/>.
+*)
 open Str
 
 module C=Map.Make (struct type t=char let compare=compare end)
