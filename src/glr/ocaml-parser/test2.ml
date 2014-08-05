69b37b004f9f549aab361ab4d45547f40385ad04
Christophe.Raffalli <Christophe.Raffalli@univ-savoie.fr>
2014-08-05 21:17:04 +0000

    fixed test for 'a instead of `a

--- src/glr/ocaml-parser/test2.ml
+++ src/glr/ocaml-parser/test2.ml
@@ -8,21 +8,21 @@ type abstract
 type bool_ = True
            | False
 
-type `a list_ = Nil
-              | Cons of `a * `a list_
+type 'a list_ = Nil
+              | Cons of 'a * 'a list_
 
-type `a tree = Empty
-             | Node of `a * `a tree * `a tree
+type 'a tree = Empty
+             | Node of 'a * 'a tree * 'a tree
 
 type inttree = int tree
 
 type coord = { x : int ; y : int ; z : int }
 
-type (`a, `b) pierce = ((`a -> `b) -> `a) -> `a
+type ('a, 'b) pierce = (('a -> 'b) -> 'a) -> 'a
 
-type (`a, `b) mod_pon = (`a -> `b) -> `a -> `b
+type ('a, 'b) mod_pon = ('a -> 'b) -> 'a -> 'b
 
-type +`a blop = Blop of `a
+type +'a blop = Blop of 'a
 
 type dummy = { mutable c : int; d : string -> bool }
 
