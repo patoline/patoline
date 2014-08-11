96a31fe5ea40f58d5d168da3795f3e0c4a8f7055
rodolphe.lepigre <rodolphe.lepigre@univ-savoie.fr>
2014-08-11 18:59:57 +0000

    Duplicated functions (pa_ocamlà)

--- src/glr/ocaml-parser/pa_ocaml.ml
+++ src/glr/ocaml-parser/pa_ocaml.ml
@@ -7,25 +7,6 @@ open Blank
 open Memoize
 open Atoms
 
-let memoize1 f = 
-  let h = Hashtbl.create 1001 in
-  (fun x ->
-     try Hashtbl.find h x with 
-       Not_found ->
-       let res = f x in
-       Hashtbl.add h x res;
-       res)
-
-let memoize2 f = 
-  let h = Hashtbl.create 1001 in
-  (fun x y ->
-     try Hashtbl.find h (x, y) with 
-       Not_found ->
-       let res = f x y in
-       Hashtbl.add h (x, y) res;
-       res)
-
-
 let fast = ref false
 let file = ref None
 let ascii = ref false
