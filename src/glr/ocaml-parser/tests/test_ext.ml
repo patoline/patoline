299b0fa66404be898546a693a8f67923340c69b8
Christophe.Raffalli <Christophe.Raffalli@univ-savoie.fr>
2014-08-11 12:34:30 +0000

    pa_ocaml: test_ext.ml: type coercion without parenthesis is less permissive (to be compatible with current ocaml)

--- src/glr/ocaml-parser/tests/test_ext.ml
+++ src/glr/ocaml-parser/tests/test_ext.ml
@@ -6,7 +6,7 @@ type t1 = A | B
 let f38b x = match x:t1 with 
   | A -> B
   | B -> A
-let f40 g x y = x:int + (g y:int):int
+let f40 g x y = (x:int) + (g y:int):int
 
 (* no parenthèses after of *)
 type (-'a,+'b) blip = Blip of 'a -> 'b
