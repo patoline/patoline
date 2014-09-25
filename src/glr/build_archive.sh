39fa97ed75386769eb9194364225f9d4e1c43faa
rodolphe.lepigre <rodolphe.lepigre@univ-savoie.fr>
2014-09-25 16:25:09 +0000

    pa_ocaml: script for generating the tarball (some files missing for now)

---
+++ src/glr/build_archive.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Compile documentation
+patoline doc/doc.typ
+
+# Create archive
+tar  --transform 's,^,/decap/,' -cvf decap.tar *.ml *.mli META Makefile all_boot.sh bootstrap/*/*.ml doc/*.ml doc/doc.pdf
