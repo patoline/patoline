77bc0b531e39efcd55ba3d02fdcc7b4fe9cc57a6
rodolphe.lepigre <rodolphe.lepigre@univ-savoie.fr>
2014-09-25 17:20:16 +0000

    pa_ocaml: added Makefile in doc directory, to compile examples

--- src/pa_ocaml/build_archive.sh
+++ src/pa_ocaml/build_archive.sh
@@ -4,4 +4,6 @@
 patoline doc/doc.typ
 
 # Create archive
-tar  --transform 's,^,/decap/,' -cvf decap.tar *.ml *.mli META Makefile all_boot.sh bootstrap/*/*.ml doc/*.ml doc/doc.pdf
+tar  --transform 's,^,/decap/,' -cvf decap.tar \
+  *.ml *.mli META Makefile all_boot.sh bootstrap/*/*.ml \
+  doc/*.ml doc/Makefile doc/doc.pdf
