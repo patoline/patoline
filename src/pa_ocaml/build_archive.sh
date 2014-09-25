d1f5e9f6f0c81fd62c12f5f9eaefd25597c3929d
rodolphe.lepigre <rodolphe.lepigre@univ-savoie.fr>
2014-09-25 19:31:51 +0000

    pa_ocaml: removed all_boot.sh from files included in the tarball

--- src/pa_ocaml/build_archive.sh
+++ src/pa_ocaml/build_archive.sh
@@ -5,6 +5,6 @@ patoline doc/doc.typ
 
 # Create archive
 tar  --transform 's,^,/decap/,' -cvf decap.tar \
-  *.ml *.mli META Makefile all_boot.sh bootstrap/*/*.ml README \
+  *.ml *.mli META Makefile bootstrap/*/*.ml README \
   doc/*.ml doc/Makefile doc/README doc/doc.pdf \
   Licence_CeCILL-B_V1-en.txt Licence_CeCILL-B_V1-fr.txt
