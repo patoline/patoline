26cf27527138c350223c6b039857b4ac0b6fc3c3
rodolphe.lepigre <rodolphe.lepigre@univ-savoie.fr>
2014-09-26 09:45:42 +0000

    pa_ocaml: copyright.ml can be compiled... (testing!)

--- src/decap/copyright.ml
+++ src/decap/copyright.ml
@@ -3,9 +3,10 @@ type comment_kind = None
                   | Multi of string * string
 
 let comment = ref None
+
 let files = ref []
 
-usage_msg = "Usage: " ^ Sys.argv.(0) ^ " [option] [LICENCE] [FILE1] [FILE2] ...\n%!"
+let usage_msg = "Usage: " ^ Sys.argv.(0) ^ " [option] [LICENCE] [FILE1] [FILE2] ...\n%!"
 
 let anon_fun s = files := !files @ [s]
 
@@ -18,33 +19,34 @@ let spec =
 let _ = Arg.parse spec anon_fun usage_msg
 
 let (licence_file, files) = match !files with
-                            | l :: fs@(f :: _) -> (l, fs)
-                            | _                -> assert false
+                            | l :: f :: fs -> (l, f :: fs)
+                            | _            -> assert false
 
 let licence filename =
   let ic = open_in filename in
   let lines = ref [] in
   try
     while true do
-      let l = read_line ic in
-      lines := l :: !lines
-    done
+      let l = input_line ic in
+      lines := l :: !lines;
+    done;
+    []
   with
-  | End_of_file -> (close_in ic; List.rev  !lines)
+  | End_of_file -> close_in ic; List.rev !lines
 
 let raw_notice = licence licence_file
 
 let header =
-  let ls = match comment with
+  let ls = match !comment with
            | None          -> raw_notice
            | Single c      -> List.map (fun l -> c ^ " " ^ l) raw_notice
-           | Multi (lc,rc) -> lc :: (List.map (fun l -> "  " ^ l) raw_notice) @ rc
+           | Multi (lc,rc) -> lc :: (List.map (fun l -> "  " ^ l) raw_notice) @ [rc]
   in (String.concat "\n" ls) ^ "\n"
 
 let add_copyright file =
   let oc = open_out_gen [Open_append] 0 file in
   seek_out oc 0;
-  output_string header;
+  output_string oc header;
   close_out oc
 
 let _ =
