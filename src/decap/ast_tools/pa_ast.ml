4062a9acca638300997a2357b9601b823b79ba4f
rodolphe.lepigre <rodolphe.lepigre@univ-savoie.fr>
2015-01-26 12:32:52 +0000

    Work on AST parser for DeCaP

---
+++ src/decap/ast_tools/pa_ast.ml
@@ -0,0 +1,141 @@
+let parser lid = | RE("[a-z][_a-z]*")
+let parser uid = | RE("[A-Z][_a-zA-Z0-9]*")
+let parser arg = | RE("['][a-z]*")
+
+type btype =
+  | Bool | Int | Char | String | Int32 | Int64 | Nativeint
+  | Option of btype
+  | List of btype
+  | Location_t | Location_loc
+  | Longident_t
+  | Var of string
+  | Loc of btype
+  | Name of string
+  | Prod of btype list
+
+type typetype =
+  | Syn of string * btype
+  | Sum of string * (string * btype option) list
+  | Rec of string option * string * (string * btype) list
+
+type ast =
+  | Type of typetype list
+  | Open of string
+
+let parser base_type =
+  | "bool"             -> Bool
+  | "int"              -> Int
+  | "char"             -> Char
+  | "string"           -> String
+  | "int32"            -> Int32
+  | "int64"            -> Int64
+  | "nativeint"        -> Nativeint
+  | "Location.t"       -> Location_t
+  | "Location.loc"     -> Location_loc
+  | a:arg              -> Var a
+  | n:lid              -> Name n
+  | "Longident.t"      -> Longident_t
+  | t:base_type DEBUG"bla" r:rest ->
+      begin
+        match r with
+        | `Option  -> Option t
+        | `List    -> List t
+        | `Loc     -> Loc t
+        | `Prod ts -> Prod (t::ts)
+      end
+  | '(' DEBUG"bla" base_type DEBUG"bli" ')'
+and rest =
+  | "option"            -> `Option
+  | "list"              -> `List
+  | "loc"               -> `Loc
+  | ts:{"*" base_type}+ -> `Prod ts
+
+let parser cdecl =
+  | uid {"of" base_type}?
+
+let parser csdecl =
+  | '|'? c:cdecl cs:{'|' cdecl}* -> c::cs
+
+let parser field =
+  | lid ':' base_type
+
+let parser fields =
+  | f:field fs:{';' field}* ';'? -> f::fs
+
+let parser any_decl =
+  | n:lid '=' cl:csdecl                                        -> Sum (n,cl)
+  | a:arg? n:lid '=' _:{arg? base_type '='}? '{' fs:fields '}' -> Rec (a,n,fs)
+  | n:lid '=' b:base_type                                      -> Syn (n,b)
+
+let parser any_rec_decl =
+  | "type" t:any_decl ts:{"and" any_decl}* -> Type (t::ts)
+  | "open" n:uid                           -> Open n
+
+let parser any_decls = | any_rec_decl*
+
+let blank = Decap.blank_regexp "[ \n\r\t]*"
+let parse = Decap.parse_file any_decls blank
+
+(*
+let rec print_quote_base = function
+  | Bool         -> "<:expr< quote_bool >>"
+  | Int          -> "<:expr< quote_int >>"
+  | Char         -> "<:expr< quote_char >>"
+  | String       -> "<:expr< quote_string >>"
+  | Int32        -> "<:expr< quote_int32 >>"
+  | Int64        -> "<:expr< quote_int64 >>"
+  | Nativeint    -> "<:expr< quote_nativeint >>"
+  | Option t     -> Printf.sprintf "<:expr< quote_option (%s) >>"
+                    (print_quote_base t)
+  | List t       -> Printf.sprintf "<:expr< quote_list (%s) >>"
+                    (print_quote_base t)
+  | Location_t   -> "<:expr< quote_Location_t >>"
+  | Location_loc -> "<:expr< quote_Location_loc >>"
+  | _            -> assert false
+
+let print_quote = function
+  | Syn (n,b)         ->
+      begin
+        Printf.printf "let quote_%s =\n%!" n;
+        Printf.printf "  %s\n\n%!" (print_quote_base b)
+      end
+  | Sum (n,cl)        ->
+      begin
+        Printf.printf "let quote_%s = function\n%!" n;
+        let print_case (c, p) =
+          match p with
+          | [] -> Printf.printf "  | %s -> <:expr< %s >>\n%!" c c
+          | _  -> Printf.printf "  | %s ... -> ...\n%!" c
+        in
+        List.iter print_case cl;
+        Printf.printf "\n%!";
+        (* TODO *)
+      end
+  | Rec (None,n,fl)   ->
+      begin
+        Printf.printf "let quote_%s =\n%!" n;
+        Printf.printf "  ...\n\n%!";
+        (* TODO *)
+      end
+  | Rec (Some a,n,fl) ->
+      begin
+        Printf.printf "let quote_%s =\n%!" n;
+        Printf.printf "  ...\n\n%!";
+        (* TODO *)
+      end
+*)
+
+let _ =
+  let grammar = any_decls in
+  let blank = Decap.blank_regexp "[ \n\r\t]*" in
+  let ast =
+    match Array.length Sys.argv with
+    | 1 -> Decap.parse_channel grammar blank stdin
+    | 2 -> Decap.parse_file grammar blank Sys.argv.(1)
+    | _ -> failwith "Wrong number of arguments..."
+  in
+  Printf.eprintf "OK! (%i)\n%!" (List.length ast)
+  (*
+  Printf.printf "let _loc = Location.none\n\n%!";
+  List.iter print_quote ast;
+  *)
