4003865bbe795ad84148b8c99434eb443e6b2534
tom.hirschowitz <tom.hirschowitz@univ-savoie.fr>
2015-05-20 08:59:22 +0000

    keks symboles dans DefaultGrammar.txp + FormatMemoire

--- src/Format/Rules.mk
+++ src/Format/Rules.mk
@@ -26,7 +26,8 @@ $(PCMX_$(d)): $(TYPOGRAPHY_DIR)/Typography.cmxa
 
 # Other formats
 OTHER_FORMATS := $(d)/FormatArticle.ml $(d)/FormatLivre.ml $(d)/FormatThese.ml \
-  $(d)/FormatLetter.ml $(d)/FormatSlides.ml $(d)/LMFormat.ml $(d)/FormatWeb.ml $(d)/Interactive.ml 
+  $(d)/FormatLetter.ml $(d)/FormatSlides.ml $(d)/LMFormat.ml $(d)/FormatWeb.ml \
+  $(d)/FormatMemoire.ml $(d)/Interactive.ml 
 
 $(OTHER_FORMATS:.ml=.cmxa): %.cmxa: %.cmx
 	$(ECHO) "[OPT]    $<"
