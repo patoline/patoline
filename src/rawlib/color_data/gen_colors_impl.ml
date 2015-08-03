c52c5ca4f61642bdce6ef50a1ce6294f5a4bb5cc
rodolphe.lepigre <rodolphe.lepigre@univ-savoie.fr>
2015-08-03 15:13:44 +0000

    Obvious bug in color generator.

--- src/rawlib/color_data/gen_colors_impl.ml
+++ src/rawlib/color_data/gen_colors_impl.ml
@@ -6,6 +6,11 @@ let _ =
 
   try while true do
     let line = input_line datafile in
-    let print = Printf.printf "let %s = rgb %i %i %i\n%!" in
+    let print n r g b =
+      let r = float_of_int r /. 255.0 in
+      let g = float_of_int g /. 255.0 in
+      let b = float_of_int b /. 255.0 in
+      Printf.printf "let %s = rgb %f %f %f\n%!" n r g b
+    in
     Scanf.sscanf line "%s #%2X%2X%2X" print
   done with End_of_file -> close_in datafile
