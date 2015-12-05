f3e3b7b83eee671343f057206ee55a240f1b347b
rodolphe.lepigre <rodolphe.lepigre@univ-savoie.fr>
2015-12-05 13:04:51 +0000

    Missing dependency (patoline needs pa_patoline)

--- src/Patoline/Rules.mk
+++ src/Patoline/Rules.mk
@@ -23,7 +23,7 @@ ifneq ($(MAKECMDGOALS),distclean)
 endif
 endif
 
-$(d)/patoline: $(TYPOGRAPHY_DIR)/Typography.cmxa $(PAT_CMX)
+$(d)/patoline: $(TYPOGRAPHY_DIR)/Typography.cmxa $(PAT_CMX) $(d)/pa_patoline
 	$(ECHO) "[OPT]    ... -> $@"
 	$(Q)$(OCAMLOPT) -o $@ $(PATOLINE_INCLUDES),threads -thread dyp.cmxa \
 		dynlink.cmxa patutil.cmxa str.cmxa unix.cmxa rbuffer.cmxa \
