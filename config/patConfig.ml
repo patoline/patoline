452bb9a837c2614840de30bcb4b0e861e1a6d4b2
rodolphe.lepigre <rodolphe.lepigre@univ-savoie.fr>
2016-04-12 22:52:12 +0000

    Some work on configuration.

---
+++ config/patConfig.ml
@@ -0,0 +1,50 @@
+open ConfigRC
+
+let fc = Filename.concat
+
+let home =
+  try Sys.getenv "XDG_DATA_HOME"
+  with Not_found -> Sys.getenv "HOME"
+
+let prefix       = "/usr/local"
+let fonts_dir    = fc prefix "share/patoline/fonts"
+let plugins_dir  = fc prefix "lib/patoline/plugins"
+let grammars_dir = fc prefix "lib/patoline/grammars"
+let hyphen_dir   = fc prefix "share/patoline/hyphen"
+
+module Spec = struct
+  open Data
+
+  (* Default Patoline configuration. *)
+  let spec =
+    [ ("fonts_dir"          , of_string fonts_dir   )
+    ; ("plugins_dir"        , of_string plugins_dir )
+    ; ("grammars_dir"       , of_string grammars_dir)
+    ; ("hyphen_dir"         , of_string grammars_dir)
+
+    ; ("extra_fonts_dirs"   , of_list String []     )
+    ; ("extra_plugins_dirs" , of_list String []     )
+    ; ("extra_grammars_dirs", of_list String []     )
+    ; ("extra_hyphen_dirs"  , of_list String []     ) ]
+
+  let name = "Patoline"
+  let path = ("/etc/patolinerc", [home ^ "/.patolinerc"; "./patolinerc"])
+end
+
+module Conf = Make(Spec)
+
+type patoconfig =
+  { fonts_dir    : string * string list
+  ; plugins_dir  : string * string list
+  ; grammars_dir : string * string list
+  ; hyphen_dir   : string * string list }
+
+let get_patoconfig : unit -> patoconfig = fun () ->
+  let cfg = Conf.get_config () in
+  let open Data in
+  let get_s k = to_string (Config.get k cfg) in
+  let get_l k = to_list String (Config.get k cfg) in
+  { fonts_dir    = (get_s "fonts_dir"   , get_l "extra_fonts_dirs"   ) 
+  ; plugins_dir  = (get_s "plugins_dir" , get_l "extra_plugins_dirs" )
+  ; grammars_dir = (get_s "grammars_dir", get_l "extra_grammars_dirs")
+  ; hyphen_dir   = (get_s "hyphen_dir"  , get_l "extra_hyphen_dirs"  ) }
