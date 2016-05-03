c58e8b20c2ff91f7d7767854ad982fe40e7f4059
rodolphe.lepigre <rodolphe.lepigre@univ-savoie.fr>
2016-05-03 05:52:13 +0000

    Dummy "Rules.mk" for the new build system (contains compilation command)

---
+++ src/patobuild2/Rules.mk
@@ -0,0 +1,3 @@
+# Dummy Rules.mk with compilation command.
+
+#ocamlfind ocamlopt -package decap,threads -thread -I ../config -pp pa_ocaml -o patoline unix.cmxa configRC.cmx patDefault.cmx patConfig.cmx str.cmxa decap.cmxa threads.cmxa patoline.ml
