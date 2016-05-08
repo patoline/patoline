2d2605ca1afbb55ee4cbb8802f1a4c51d2727de6
rodolphe.lepigre <rodolphe.lepigre@univ-savoie.fr>
2016-05-08 17:26:45 +0000

    Fixed printf

--- src/patobuild2/parallel.ml
+++ src/patobuild2/parallel.ml
@@ -2,12 +2,11 @@
 let nb_threads = ref 1
 
 (* Thread-safe printing functions. Be careful to always FULLY APPLY them. *)
-let fprintf : out_channel -> ('a, out_channel, unit) format -> 'a = fun och ->
-  let m = Mutex.create () in
-  let fprintf fmt =
-    Mutex.lock m;
-    Printf.kfprintf (fun _ -> Mutex.unlock m) och fmt
-  in Obj.magic fprintf
+let m = Mutex.create ()
+let fprintf : out_channel -> ('a, out_channel, unit) format -> 'a =
+  fun och fmt ->
+    let f _ _ = Mutex.lock m in
+    Printf.kfprintf (fun _ -> Mutex.unlock m) och ("%a" ^^ fmt) f ()
 
 let printf  : ('a, out_channel, unit) format -> 'a = fun fmt ->
   fprintf stdout fmt
